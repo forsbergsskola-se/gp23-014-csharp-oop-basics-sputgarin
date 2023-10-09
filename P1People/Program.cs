e4e4e672224163d73d5dbd6d0c1e9eb80f30de1d
sputgarin <sputgarin@protonmail.com>
2023-10-09 14:19:13 +0200

    P1People & P1_1Classes Completed

---
+++ P1People/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// Creates an array. 
+Person[] person = new Person[3];
+
+// Instanziation of an object of a class
+person[0] = new Person();
+person[1] = new Person();
+person[2] = new Person();
+
+Print(person[0]);
+Print(person[1]);
+Print(person[2]);
+
+void Print(Person person)
+{
+    Console.WriteLine(person);
+}
+
+
+// Class declaration
+public class Person
+{
+    
+}
+
---
+++ P1_1Classes/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Person person = new Person();
+Animal animal = new Animal();
+Car car = new Car();
+
+Print(person, animal, car);
+
+void Print(Person person, Animal animal, Car car)
+{
+    Console.WriteLine(person);
+    Console.WriteLine(animal);
+    Console.WriteLine(car);
+}
+
+public class Person
+{
+    
+}
+
+public class Animal
+{
+    
+}
+
+public class Car
+{
+    
+}
