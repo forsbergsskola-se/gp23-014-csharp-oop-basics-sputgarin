3494a587e1c6dab290b621f46f73175dd5a53193
sputgarin <sputgarin@protonmail.com>
2023-10-11 09:58:08 +0200

    P3_3OpenDoor Completed

---
+++ P3_3OpenDoor/House.cs
@@ -0,0 +1,16 @@
+﻿namespace P3_3OpenDoor;
+
+public class House
+{
+    public bool door = false;
+
+    public void OpenDoor()
+    {
+        door = true;
+    }
+    public void CloseDoor()
+    {
+        door = false;
+    }
+    
+}
---
+++ P3_3OpenDoor/Program.cs
@@ -0,0 +1,10 @@
+﻿using P3_3OpenDoor;
+
+House redHouse = new House();
+House blueHouse  = new House();
+
+Console.WriteLine($"Red house door is: {redHouse.door} Blue house door is: {blueHouse.door}");
+blueHouse.OpenDoor();
+Console.WriteLine($"Red house door is: {redHouse.door} Blue house door is: {blueHouse.door}");
+redHouse.OpenDoor();
+Console.WriteLine($"Red house door is: {redHouse.door} Blue house door is: {blueHouse.door}");
